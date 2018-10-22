4e82d555c696e310d1540d2d26b990240761ea87
Rudolf Konstantinov <korund93@gmail.com>
2018-10-23 02:19:34 +0300

    Updates

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
     }
+    AppComponent.prototype.ngOnInit = function () { };
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-root',
