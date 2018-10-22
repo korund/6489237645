ad7342d9c55dd64e3e1f8fe51cfa2565012d0767
Rudolf Konstantinov <korund93@gmail.com>
2018-10-23 01:50:17 +0300

    unit tests added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,34 @@
+import {Component} from "@angular/core";
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockNavComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'apptest'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('apptest');
-  }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render app-item component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to apptest!');
+    expect(compiled.querySelector('app-item')).not.toBeNull();
   }));
 });
+
+@Component({
+  selector: 'app-item',
+  template: ''
+})
+class MockNavComponent {
+}
--- src/grid-element/grid-element.component.spec.ts
+++ src/grid-element/grid-element.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GridElementComponent } from './grid-element.component';
+
 
 describe('GridElementComponent', () => {
   let component: GridElementComponent;
@@ -15,11 +15,37 @@ describe('GridElementComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GridElementComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it(`should have as title 'Item title'`, async(() => {
+    const title = 'Item title';
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(title);
+  }));
+
+  it('should have an image', function () {
+    const imagePath = './assets/sample.jpg';
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('img').attributes['src'].value).toBe(imagePath);
+  });
+
+  it('should have a number of paragraphs by length of content array', function () {
+    const contentLength = 5;
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('p').length).toBe(contentLength);
+  });
+
+  it('should have a button', function () {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button')).not.toBeNull();
+  });
+
+  it('should call clicked() on button click', function () {
+    spyOn(component, 'clicked');
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    expect(component.clicked).toHaveBeenCalled();
   });
 });
